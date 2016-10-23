e9f6cc70b5d3f35e403517ba664bc88195b233e3
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-23 11:13:07 +0530

    Show limit dunation message

--- includes/filters.php
+++ includes/filters.php
@@ -20,7 +20,17 @@ function give_ldd_form_close( $is_closed, $form_id ) {
 
 add_filter( 'give_is_close_donation_form', 'give_ldd_form_close', 10, 2 );
 
-// function give_ldd_closed_form_message(){
-//
-// }
-// add_filter( 'give_goal_closed_output', 'give_ldd_closed_form_message' );
+function give_ldd_closed_form_message( $message, $form_id ){
+	$form = new Give_Donate_Form( $form_id );
+
+	// If form is for limited duration and goal is not achieved then show limit duration message.
+	if(
+		give_is_limit_donation_time_achieved( $form_id )
+		&& ( $form->get_goal() > $form->get_earnings() )
+	) {
+		$message = get_post_meta( $form_id, 'limit-donation-message', true );
+	}
+
+	return $message;
+}
+add_filter( 'give_goal_closed_output', 'give_ldd_closed_form_message', 9999, 2 );
