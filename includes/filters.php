1af80092133cae4c5b2158d7092cfdbb90395bd6
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-23 11:13:28 +0530

    Improve code formatting

--- includes/filters.php
+++ includes/filters.php
@@ -20,11 +20,11 @@ function give_ldd_form_close( $is_closed, $form_id ) {
 
 add_filter( 'give_is_close_donation_form', 'give_ldd_form_close', 10, 2 );
 
-function give_ldd_closed_form_message( $message, $form_id ){
+function give_ldd_closed_form_message( $message, $form_id ) {
 	$form = new Give_Donate_Form( $form_id );
 
 	// If form is for limited duration and goal is not achieved then show limit duration message.
-	if(
+	if (
 		give_is_limit_donation_time_achieved( $form_id )
 		&& ( $form->get_goal() > $form->get_earnings() )
 	) {
@@ -33,4 +33,5 @@ function give_ldd_closed_form_message( $message, $form_id ){
 
 	return $message;
 }
+
 add_filter( 'give_goal_closed_output', 'give_ldd_closed_form_message', 9999, 2 );
