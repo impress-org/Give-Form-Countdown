02ae49bfbc0f91d0a298ec71d7e448fa4d330f11
Matt Cromwell <reachme@mattcromwell.com>
2020-03-26 21:39:31 -0700

    Use get_post_meta for every settings instead of grabbing ALL post_meta.

--- template/countdown_clock_1.php
+++ template/countdown_clock_1.php
@@ -5,39 +5,39 @@
  * @since 1.0
  */
 
-$form_meta = get_post_meta($form_id);
-$theme = $form_meta['form-countdown-theme'][0];
-$custom_styles = gfc_output_custom_color_scheme($form_id);
+$form_meta = get_post_meta( $form_id );
+$theme = get_post_meta( $form_id, 'form-countdown-theme', true );
+$custom_styles = gfc_output_custom_color_scheme( $form_id );
 
-$date = $form_meta['form-countdown-on-date'][0] . ' ' . $form_meta['form-countdown-on-time'][0];
-$closeaction = $form_meta['form-countdown-achieved-action'][0];
+$date = get_post_meta( $form_id, 'form-countdown-on-date', true ) . ' ' . get_post_meta($form_id, 'form-countdown-on-time', true );
+$closeaction = get_post_meta( $form_id, 'form-countdown-achieved-action', true );
 
 //Create a new DateTime object using the date string above.
-$dateTime = new DateTime($date);
+$dateTime = new DateTime( $date );
  
 //Format it into a Unix timestamp.
 $timestamp = $dateTime->format('U');
 
-if ($theme == 'custom') {
+if ( $theme == 'custom' ) {
 	echo $custom_styles;
 }
 
 ?>
 <div id="gfc-clock-<?php echo esc_attr( $form_id ); ?>-wrap" class="gfc-clock-wrap">
 	<div id="flipdown-<?php echo esc_attr( $form_id ); ?>" class="flipdown"></div>
-	<div class="gfc-closed-message" style="display:none; margin: 1em 0; overflow:hidden;"><?php echo wp_kses_post( $form_meta['form-countdown-message'][0] ); ?></div>
+	<div class="gfc-closed-message" style="display:none; margin: 1em 0; overflow:hidden;"><?php echo wp_kses_post( get_post_meta( $form_id, 'form-countdown-message', true ) ); ?></div>
 </div>
 
 <script>
 	document.addEventListener('DOMContentLoaded', () => {
 
-		var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1,
-			fiveSeconds = (new Date().getTime() / 1000) + 5,
+		var twoDaysFromNow = ( new Date().getTime() / 1000 ) + (86400 * 2) + 1,
+			fiveSeconds = ( new Date().getTime() / 1000 ) + 5,
 			closedMessage = 
-			formwrap = document.getElementById("give-form-<?php echo $form_id?>-wrap");
+			formwrap = document.getElementById("give-form-<?php echo esc_attr( $form_id ); ?>-wrap");
 		
-		//Testing
-		var flipdown = new FlipDown(fiveSeconds,'flipdown-<?php echo $form_id; ?>', {theme: '<?php echo esc_attr( $theme ); ?>'})
+		//5 second countdown for testing only
+		var flipdown = new FlipDown(fiveSeconds,'flipdown-<?php echo esc_attr( $form_id ); ?>', {theme: '<?php echo esc_attr( $theme ); ?>'})
 
 		//var flipdown = new FlipDown(<?php echo esc_attr( $timestamp ); ?>,'flipdown-<?php echo esc_attr( $form_id ); ?>', {theme: '<?php echo esc_attr( $theme ); ?>'})
 
